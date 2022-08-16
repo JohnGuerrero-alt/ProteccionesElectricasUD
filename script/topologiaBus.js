bd0cc813e40e8096ce9c9032a9d952f2d0c81a5a
JohnGuerrero-alt <76411006+JohnGuerrero-alt@users.noreply.github.com>
2022-08-16 21:52:49 +0200

    Add files via upload

--- script/topologiaBus.js
+++ script/topologiaBus.js
@@ -503,8 +503,8 @@ async function ajustarValoresCoordinacion(Rele) {
                 <table class="table align-middle" style="font-size:13px">
                     <tr>
                         <th scope="col">Tiempo local <br> (seg)</th>
-                        <td><input type="number" class="swal2-input"   value="${modeloRele[Rele].tiempoLocal}" min="0"/> </td>
-                        <td><div class="botonGuardar" onclick="calcularNuevoDial(${Rele})">Calcular nuevo <br> Dial</div></td>
+                        <td><input type="number" class="swal2-input" id="swal-tiempoLocal"  value="${modeloRele[Rele].tiempoLocal}" min="0"/> </td>
+                        <td><div class="botonGuardar hoverButton" style="height:50px; width: 100px" onclick="calcularNuevoDial(${Rele})">Calcular nuevo Dial</div></td>
                     </tr>
                     <tr class="table-secondary">
                         <th scope="row"></th>
@@ -512,22 +512,218 @@ async function ajustarValoresCoordinacion(Rele) {
                     </tr>
                     <tr>
                         <th scope="col">Dial <br> Calculado</th>
-                        <td colspan="2">${modeloRele[Rele].TMS_calculado}</td>
+                        <td colspan="2" id="swal-tmsCalculado">${modeloRele[Rele].TMS_calculado}</td>
                         
                     </tr>
                 </table>
             </div>
             
         `,
+        showConfirmButton: true,
+        confirmButtonText: "Guardar",
+        showCloseButton: true,
+        showCancelButton: false,
+        customClass: {
+            confirmButton: 'botonGuardar hoverButton',
+        },
+        preConfirm: () => {
+            return [
+                document.getElementById('swal-tiempoLocal').value,
+                document.getElementById('swal-tmsCalculado').innerText,
+                modeloRele[Rele].tiempoLocal
+                
+            ]
+        },
 
     })
+
+    if (formValues) {
+        console.log("whattttt :", formValues) 
+        document.getElementById("Tiempo_local_"+Rele).innerText = parseFloat(formValues[0]);
+        document.getElementById("Dial_calculado_"+Rele).innerText = parseFloat(formValues[1]);
+        modeloRele[Rele].tiempoLocal = parseFloat(formValues[0]);
+    
+    }
 
 
 }
 
 //Función para calcular el nuevo DIAL
 function calcularNuevoDial(Rele) {
+    //Establecemos parametros importantes
+    Isc_ReleA = modeloRele[0].Isc;
+    Isc_ReleB = modeloRele[1].Isc;
+    Isc_ReleC = modeloRele[2].Isc;
+    Ic_ReleA = modeloRele[0].Ic;
+    Ic_ReleB = modeloRele[1].Ic;
+    Ic_ReleC = modeloRele[2].Ic;
+    Margen_Ic_A = modeloRele[0].margenIc;
+    Margen_Ic_B = modeloRele[1].margenIc;
+    Margen_Ic_C = modeloRele[2].margenIc;
+    familiaCurva_A = modeloRele[0].familiaCurva;
+    familiaCurva_B = modeloRele[1].familiaCurva;
+    familiaCurva_C = modeloRele[2].familiaCurva;
+    TMS_A = modeloRele[0].TMS;
+    TMS_B = modeloRele[1].TMS;
+    TMS_C = modeloRele[2].TMS;
+    tiempo_A = modeloRele[0].tiempo;
+    tiempo_B = modeloRele[1].tiempo;
+    if(Rele == 2) {
+        tiempo_C = (document.getElementById("swal-tiempoLocal").value)*1000;
+        console.log("Tiempooooo local: ", tiempo_C)
+    }
+    if(Rele == 1) {
+        tiempo_B = (document.getElementById("swal-tiempoLocal").value)*1000;
+        console.log("Tiempooooo local: ", tiempo_B)
+    }
+    if(Rele == 0) {
+        tiempo_A = (document.getElementById("swal-tiempoLocal").value)*1000;
+        console.log("Tiempooooo local: ", tiempo_A)
+    }
     
+
+   
+    tiempo_instantaneo_A = modeloRele[0].tiempoInstantaneo;
+    tiempo_instantaneo_B = modeloRele[1].tiempoInstantaneo;
+    tiempo_instantaneo_C = modeloRele[2].tiempoInstantaneo; 
+
+    try {
+        Ic_c_con_margen = Ic_ReleC + (Ic_ReleC) * (Margen_Ic_C / 100)
+
+        //DIAL en el relé C
+        TMSparteIEC_C = parametrosCurva[familiaCurva_C].IEC * ((tiempo_C / 1000) * (((Math.pow(Isc_ReleC / Ic_c_con_margen, parametrosCurva[familiaCurva_C].alfa)) - 1) / (parametrosCurva[familiaCurva_C].beta)));
+        console.log("nuevo tiempo: ", tiempo_C)
+
+
+        TMSparteANSI_C = parametrosCurva[familiaCurva_C].ANSI * ((tiempo_C / 1000) * 1 / ((parametrosCurva[familiaCurva_C].A) + (parametrosCurva[familiaCurva_C].B / ((Isc_ReleC / Ic_c_con_margen) - parametrosCurva[familiaCurva_C].C)) + (parametrosCurva[familiaCurva_C].D / (Math.pow((Isc_ReleC / Ic_c_con_margen) - parametrosCurva[familiaCurva_C].C, 2))) + (parametrosCurva[familiaCurva_C].E / (Math.pow((Isc_ReleC / Ic_c_con_margen) - parametrosCurva[familiaCurva_C].C, 3)))));
+
+        console.log("parteIEC C: ", TMSparteIEC_C);
+        console.log("parteANSI C: ", TMSparteANSI_C)
+
+
+        TMS_calculado_C = (TMSparteIEC_C + TMSparteANSI_C).toFixed(4);
+        modeloRele[2].TMS_calculado = TMS_calculado_C;
+        console.log("TMS_calculado_C", TMS_calculado_C)
+
+        document.getElementById('swal-tmsCalculado').innerText = TMS_calculado_C
+       
+
+        // if (isNaN(TMS_calculado_C)) { TMS_calculado_C = "-" }
+        // document.getElementById("Dial_calculado_2").innerText = TMS_calculado_C;
+
+        //tLocalparteIEC_C = (parametrosCurva[familiaCurva_C].IEC * TMS_C * ((parametrosCurva[familiaCurva_C].beta) / ((Math.pow(Isc_ReleC / Ic_c_con_margen, parametrosCurva[familiaCurva_C].alfa)) - 1)));
+
+        //tLocalparteANSI_C = (parametrosCurva[familiaCurva_C].ANSI * TMS_C * ((parametrosCurva[familiaCurva_C].A) + (parametrosCurva[familiaCurva_C].B / ((Isc_ReleC / Ic_c_con_margen) - (parametrosCurva[familiaCurva_C].C)) + (parametrosCurva[familiaCurva_C].D / (Math.pow((Isc_ReleC / Ic_c_con_margen) - (parametrosCurva[familiaCurva_C].C), 2))) + (parametrosCurva[familiaCurva_C].E / (Math.pow((Isc_ReleC / Ic_c_con_margen) - (parametrosCurva[familiaCurva_C].C), 3))))));
+
+
+
+        //Hallar tiempo Local en C
+        //tiempo_local_C = (tLocalparteIEC_C + tLocalparteANSI_C).toFixed(4);
+        //modeloRele[2].tiempoLocal = tiempo_local_C;
+        //console.log("tiempo local C: ", tiempo_local_C)
+
+        // if (isNaN(tiempo_local_C)) { tiempo_local_C = "__" }
+        // document.getElementById("Tiempo_local_2").innerHTML = tiempo_local_C;
+
+
+
+
+
+    } catch (error) {
+
+    }
+
+    try {
+        Ic_b_con_margen = Ic_ReleB + (Ic_ReleB) * (Margen_Ic_B / 100)
+
+        //DIAL en el relé B
+        TMSparteIEC_B = parametrosCurva[familiaCurva_B].IEC * ((tiempo_B / 1000) * (((Math.pow(Isc_ReleB / Ic_b_con_margen, parametrosCurva[familiaCurva_B].alfa)) - 1) / (parametrosCurva[familiaCurva_B].beta)));
+
+        TMSparteANSI_B = parametrosCurva[familiaCurva_B].ANSI * ((tiempo_B / 1000) * 1 / ((parametrosCurva[familiaCurva_B].A) + (parametrosCurva[familiaCurva_B].B / ((Isc_ReleB / Ic_b_con_margen) - parametrosCurva[familiaCurva_B].C)) + (parametrosCurva[familiaCurva_B].D / (Math.pow((Isc_ReleB / Ic_b_con_margen) - parametrosCurva[familiaCurva_B].C, 2))) + (parametrosCurva[familiaCurva_B].E / (Math.pow((Isc_ReleB / Ic_b_con_margen) - parametrosCurva[familiaCurva_B].C, 3)))));
+
+
+        TMS_calculado_B = (TMSparteIEC_B + TMSparteANSI_B).toFixed(4);
+        modeloRele[1].TMS_calculado =  TMS_calculado_B;
+        if (isNaN(TMS_calculado_B)) { TMS_calculado_B = "-" }
+
+
+        document.getElementById('swal-tmsCalculado').innerText = TMS_calculado_B
+
+
+        document.getElementById("Dial_calculado_1").innerText = TMS_calculado_B;
+
+        //tLocalparteIEC_B = (parametrosCurva[familiaCurva_B].IEC * TMS_B * ((parametrosCurva[familiaCurva_B].beta) / ((Math.pow(Isc_ReleB / Ic_b_con_margen, parametrosCurva[familiaCurva_B].alfa)) - 1)));
+
+        //tLocalparteANSI_B = (parametrosCurva[familiaCurva_B].ANSI * TMS_B * ((parametrosCurva[familiaCurva_B].A) + (parametrosCurva[familiaCurva_B].B / ((Isc_ReleB / Ic_b_con_margen) - (parametrosCurva[familiaCurva_B].C)) + (parametrosCurva[familiaCurva_B].D / (Math.pow((Isc_ReleB / Ic_b_con_margen) - (parametrosCurva[familiaCurva_B].C), 2))) + (parametrosCurva[familiaCurva_B].E / (Math.pow((Isc_ReleB / Ic_b_con_margen) - (parametrosCurva[familiaCurva_B].C), 3))))));
+
+
+        //Hallar tiempo local en B
+        //tiempo_local_B = (tLocalparteIEC_B + tLocalparteANSI_B).toFixed(4);
+        //modeloRele[1].tiempoLocal = tiempo_local_B;
+        //if (isNaN(tiempo_local_B)) { tiempo_local_B = "__" }
+        //document.getElementById("Tiempo_local_1").innerHTML = tiempo_local_B;
+
+
+        //Hallar tiempo remoto en B
+        //tRemotoparteIEC_B = (parametrosCurva[familiaCurva_B].IEC * TMS_B * ((parametrosCurva[familiaCurva_B].beta) / ((Math.pow(Isc_ReleC / Ic_b_con_margen, parametrosCurva[familiaCurva_B].alfa)) - 1)));
+
+        //tRemotoparteANSI_B = (parametrosCurva[familiaCurva_B].ANSI * TMS_B * ((parametrosCurva[familiaCurva_B].A) + (parametrosCurva[familiaCurva_B].B / ((Isc_ReleC / Ic_b_con_margen) - (parametrosCurva[familiaCurva_B].C)) + (parametrosCurva[familiaCurva_B].D / (Math.pow((Isc_ReleC / Ic_b_con_margen) - (parametrosCurva[familiaCurva_B].C), 2))) + (parametrosCurva[familiaCurva_B].E / (Math.pow((Isc_ReleC / Ic_b_con_margen) - (parametrosCurva[familiaCurva_B].C), 3))))));
+
+
+        //tiempo_remoto_B = (tRemotoparteIEC_B + tRemotoparteANSI_B).toFixed(4);
+        //if (isNaN(tiempo_remoto_B)) { tiempo_remoto_B = "__" }
+        //document.getElementById("Tiempo_remoto_1").innerHTML = tiempo_remoto_B;
+
+    } catch (error) {
+
+    }
+
+    try {
+
+        //DIAL en el relé A
+        Ic_a_con_margen = Ic_ReleA + (Ic_ReleA) * (Margen_Ic_A / 100)
+
+        TMSparteIEC_A = parametrosCurva[familiaCurva_A].IEC * ((tiempo_A / 1000) * (((Math.pow(Isc_ReleA / Ic_a_con_margen, parametrosCurva[familiaCurva_A].alfa)) - 1) / (parametrosCurva[familiaCurva_A].beta)));
+
+        TMSparteANSI_A = parametrosCurva[familiaCurva_A].ANSI * ((tiempo_A / 1000) * 1 / ((parametrosCurva[familiaCurva_A].A) + (parametrosCurva[familiaCurva_A].B / ((Isc_ReleA / Ic_a_con_margen) - parametrosCurva[familiaCurva_A].C)) + (parametrosCurva[familiaCurva_A].D / (Math.pow((Isc_ReleA / Ic_a_con_margen) - parametrosCurva[familiaCurva_A].C, 2))) + (parametrosCurva[familiaCurva_A].E / (Math.pow((Isc_ReleA / Ic_a_con_margen) - parametrosCurva[familiaCurva_A].C, 3)))));
+
+
+        TMS_calculado_A = (TMSparteANSI_A + TMSparteIEC_A).toFixed(4);
+        modeloRele[0].TMS_calculado = TMS_calculado_A;
+        if (isNaN(TMS_calculado_A)) { TMS_calculado_A = "-" }
+
+        document.getElementById('swal-tmsCalculado').innerText = TMS_calculado_A;
+
+        document.getElementById("Dial_calculado_0").innerText = TMS_calculado_A;
+
+        //Hallar tiempo local en A
+        //tLocalparteIEC_A = (parametrosCurva[familiaCurva_A].IEC * TMS_A * ((parametrosCurva[familiaCurva_A].beta) / ((Math.pow(Isc_ReleA / Ic_a_con_margen, parametrosCurva[familiaCurva_A].alfa)) - 1)));
+
+
+        //tLocalparteANSI_A = (parametrosCurva[familiaCurva_A].ANSI * TMS_A * ((parametrosCurva[familiaCurva_A].A) + (parametrosCurva[familiaCurva_A].B / ((Isc_ReleA / Ic_a_con_margen) - (parametrosCurva[familiaCurva_A].C)) + (parametrosCurva[familiaCurva_A].D / (Math.pow((Isc_ReleA / Ic_a_con_margen) - (parametrosCurva[familiaCurva_A].C), 2))) + (parametrosCurva[familiaCurva_A].E / (Math.pow((Isc_ReleA / Ic_a_con_margen) - (parametrosCurva[familiaCurva_A].C), 3))))));
+
+       
+
+
+        //Hallar tiempo remoto en A
+        //tRemotoparteIEC_A = (parametrosCurva[familiaCurva_A].IEC * TMS_A * ((parametrosCurva[familiaCurva_A].beta) / ((Math.pow(Isc_ReleB / Ic_a_con_margen, parametrosCurva[familiaCurva_A].alfa)) - 1)));
+
+        //tRemotoparteANSI_A = (parametrosCurva[familiaCurva_A].ANSI * TMS_A * ((parametrosCurva[familiaCurva_A].A) + (parametrosCurva[familiaCurva_A].B / ((Isc_ReleB / Ic_a_con_margen) - (parametrosCurva[familiaCurva_A].C)) + (parametrosCurva[familiaCurva_A].D / (Math.pow((Isc_ReleB / Ic_a_con_margen) - (parametrosCurva[familiaCurva_A].C), 2))) + (parametrosCurva[familiaCurva_A].E / (Math.pow((Isc_ReleB / Ic_a_con_margen) - (parametrosCurva[familiaCurva_A].C), 3))))));
+
+        //tiempo_remoto_A = (tRemotoparteIEC_A + tRemotoparteANSI_A).toFixed(4);
+
+        //if (isNaN(tiempo_remoto_A)) { tiempo_remoto_A = "__" }
+        //document.getElementById("Tiempo_remoto_0").innerHTML = tiempo_remoto_A;
+
+
+    } catch (error) {
+        document.getElementById("Dial_calculado_0").innerText = "";
+    }
+
+
+    
+
+
 
 }
 
@@ -605,6 +801,8 @@ function calcularPasoaPaso() {
 
         //DIAL en el relé C
         TMSparteIEC_C = parametrosCurva[familiaCurva_C].IEC * ((tiempo_C / 1000) * (((Math.pow(Isc_ReleC / Ic_c_con_margen, parametrosCurva[familiaCurva_C].alfa)) - 1) / (parametrosCurva[familiaCurva_C].beta)));
+        console.log("nuevo tiempo: ", tiempo_C)
+
 
         TMSparteANSI_C = parametrosCurva[familiaCurva_C].ANSI * ((tiempo_C / 1000) * 1 / ((parametrosCurva[familiaCurva_C].A) + (parametrosCurva[familiaCurva_C].B / ((Isc_ReleC / Ic_c_con_margen) - parametrosCurva[familiaCurva_C].C)) + (parametrosCurva[familiaCurva_C].D / (Math.pow((Isc_ReleC / Ic_c_con_margen) - parametrosCurva[familiaCurva_C].C, 2))) + (parametrosCurva[familiaCurva_C].E / (Math.pow((Isc_ReleC / Ic_c_con_margen) - parametrosCurva[familiaCurva_C].C, 3)))));
 
